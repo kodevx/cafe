9e4788fcff8c5ad117c555985d25288c03b2b249
kodevx <rohithunni19@yahoo.com>
2019-08-28 13:00:52 +0530

    Updates

---
+++ precache-manifest.638d84c12e913be9e4b79fca8a39145a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b20c4ca1ffadea7db2875c854b4f7ed5",
+    "url": "/cafe/index.html"
+  },
+  {
+    "revision": "c7209d1eb325222f9ce1",
+    "url": "/cafe/static/css/main.3907ecec.chunk.css"
+  },
+  {
+    "revision": "f85c1718dd67e05782a5",
+    "url": "/cafe/static/js/2.8563ccd9.chunk.js"
+  },
+  {
+    "revision": "c7209d1eb325222f9ce1",
+    "url": "/cafe/static/js/main.9d87c69c.chunk.js"
+  },
+  {
+    "revision": "67d30617a734fc596bae",
+    "url": "/cafe/static/js/runtime~main.4685ec86.js"
+  },
+  {
+    "revision": "5959bf87458d7800c8615d3d676abd7d",
+    "url": "/cafe/static/media/Brillotus.5959bf87.ttf"
+  },
+  {
+    "revision": "9c35bf87f23c8cca614720126fe0baa0",
+    "url": "/cafe/static/media/Gotham-Bold.9c35bf87.otf"
+  },
+  {
+    "revision": "f76e3adf545b3299f643fd7642800351",
+    "url": "/cafe/static/media/Gotham-Light.f76e3adf.otf"
+  },
+  {
+    "revision": "77171d8f5b5283f9d47a3434704bf944",
+    "url": "/cafe/static/media/GothamMedium_0.77171d8f.ttf"
+  },
+  {
+    "revision": "a9974305e13683a23a4b5a8ec8a946cd",
+    "url": "/cafe/static/media/amc.a9974305.jpg"
+  },
+  {
+    "revision": "3dba235513a683a77bc4a46e991194c3",
+    "url": "/cafe/static/media/back.3dba2355.jpeg"
+  },
+  {
+    "revision": "7aee1ea927de13ca7107b3ebae83130b",
+    "url": "/cafe/static/media/brazilian.7aee1ea9.jpg"
+  },
+  {
+    "revision": "392fc1786751c2000cf3216e78957bd1",
+    "url": "/cafe/static/media/cap.392fc178.png"
+  },
+  {
+    "revision": "1cb575641f4c405e0cb2aae12435a2bc",
+    "url": "/cafe/static/media/espresso.1cb57564.png"
+  },
+  {
+    "revision": "c472399db70d4404c1e9ffb635b6aeb5",
+    "url": "/cafe/static/media/latte.c472399d.png"
+  },
+  {
+    "revision": "a9b240e95f0e47ab87d7bc1d55a9f764",
+    "url": "/cafe/static/media/mocha.a9b240e9.png"
+  },
+  {
+    "revision": "4e20f84f62be1d3522475cc4368c54aa",
+    "url": "/cafe/static/media/tcbo.4e20f84f.jpg"
+  }
+]);
